0c5f4a45495e30fb2ea14e893b4a5da1fb1040fd
JP Hastings-Spital <jphastings@blinkbox.com>
2015-01-29 10:55:04 +0000

    Fix Travis using 1.9.3 to build gems. Again.

--- lib/blinkbox/common_messaging/version.rb
+++ lib/blinkbox/common_messaging/version.rb
@@ -1,7 +1,7 @@
 module Blinkbox
   module CommonMessaging
     VERSION = begin
-      File.read(File.join(__dir__, "../../../VERSION"))
+      File.read(File.join(File.dirname(__FILE__), "../../../VERSION"))
     rescue Errno::ENOENT
       "0.0.0-unknown"
     end
